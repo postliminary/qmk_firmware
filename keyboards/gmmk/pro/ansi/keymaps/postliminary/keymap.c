65cff28c7befe692da9f8c24a6d65b86e6c826f8
postliminary <6505930+postliminary@users.noreply.github.com>
2023-10-26 16:12:42 -0400

    Update modtaps for gmmk pro keymap

--- keyboards/gmmk/pro/ansi/keymaps/postliminary/keymap.c
+++ keyboards/gmmk/pro/ansi/keymaps/postliminary/keymap.c
@@ -44,7 +44,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,          KC_DEL,
      MO(_CAPS),  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,           KC_HOME,
         KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT, KC_UP,   KC_END,
-        KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                             KC_RALT, MO(_FN), KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
+        KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                             KC_RALT, KC_RGUI, MO(_FN), KC_LEFT, KC_DOWN, KC_RGHT
     ),
 
     [_HHKB] = LAYOUT(
@@ -53,16 +53,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSPC,          KC_END,
         KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,           KC_PGUP,
         KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT, KC_UP,   KC_PGDN,
-        KC_LGUI, ___X___, KC_LALT,                            KC_SPC,                             KC_RALT, ___X___, MO(_FN), KC_LEFT, KC_DOWN, KC_RGHT
+        ___X___, KC_LGUI, KC_LALT,                            LT(_FN, KC_SPC),                    KC_RALT, KC_RGUI, ___X___, KC_LEFT, KC_DOWN, KC_RGHT
     ),
 
     [_FN] = LAYOUT(
-        ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___,          KC_MPLY,
-        ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___,          KC_MPRV,
-        ___X___, ___X___, KC_WH_U, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, KC_PSCR, ___X___, ___X___, ___X___,          KC_MNXT,
-        ___X___, KC_WH_L, KC_WH_D, KC_WH_R, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___,          ___X___,          KC_VOLU,
-        ___X___,          ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___,          ___X___, ___X___, KC_VOLD,
-        ___X___, ___X___, ___X___,                            ___X___,                           MO(_KBFN),_______, _______, ___X___, ___X___, ___X___
+        _______, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___,          _______,
+        _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,   KC_F13,          _______,
+        _______, ___X___, KC_WH_U, ___X___, ___X___, ___X___, ___X___, KC_HOME, KC_UP,   KC_END,  KC_PGUP, KC_VOLD, KC_VOLU,  KC_DEL,          _______,
+        _______, KC_WH_L, KC_WH_D, KC_WH_R, ___X___, KC_PSCR, ___X___, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, ___X___,          _______,          _______,
+        _______,          ___X___, ___X___, KC_CAPS, ___X___, ___X___, ___X___, ___X___, KC_MPRV, KC_MNXT, KC_MPLY,          _______, _______, _______,
+        _______, _______, _______,                            _______,                            _______,MO(_KBFN),_______, _______, _______, _______
     ),
 
     [_CAPS] = LAYOUT(
@@ -75,12 +75,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     [_KBFN] = LAYOUT(
-        BOOT_AT, RGB_HUD, RGB_HUI, RGB_SAD, RGB_SAI, RGB_SPD, RGB_SPI,RGB_RMOD, RGB_MOD, LO_TOGG, RGB_VAD, RGB_VAI, RGB_TOG, ___X___,          ___X___,
-        NK_TOGG, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___,          ___X___,
-        ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___,          ___X___,
-        ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, HH_TOGG, ___X___, ___X___, ___X___, ___X___, ___X___,          ___X___,          ___X___,
-        ___X___,          ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___,          ___X___, ___X___, ___X___,
-        ___X___, ___X___, ___X___,                            ___X___,                            _______, _______, _______, ___X___, ___X___, ___X___
+        BOOT_AT, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___,          ___X___,
+        ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, LO_TOGG, RGB_RMOD,RGB_MOD, RGB_VAD, RGB_VAI, RGB_TOG,          ___X___,
+        ___X___, ___X___, RGB_SAI, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, RGB_SPD, RGB_SPI, ___X___,          ___X___,
+        ___X___, RGB_HUD, RGB_SAD, RGB_HUI, ___X___, ___X___, HH_TOGG, ___X___, ___X___, ___X___, ___X___, ___X___,          ___X___,          ___X___,
+        ___X___,          ___X___, ___X___, ___X___, ___X___, ___X___, NK_TOGG, ___X___, ___X___, ___X___, ___X___,          ___X___, ___X___, ___X___,
+        _______, _______, _______,                            _______,                            _______, _______, _______, ___X___, ___X___, ___X___
     ),
 };
 // clang-format on
